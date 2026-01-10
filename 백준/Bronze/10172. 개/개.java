636095d74ea3238af0996e38d3b9cb1c1380166d
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-01-11 00:28:21 +0900

    [Bronze V] Title: 개, Time: 60 ms, Memory: 11384 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append("|\\_/|").append("\n")
+                .append("|q p|   /}").append("\n")
+                .append("( 0 )\"\"\"\\").append("\n")
+                .append("|\"^\"`    |").append("\n")
+                .append("||_/=\\\\__|");
+        System.out.println(sb);
+    }
+}
