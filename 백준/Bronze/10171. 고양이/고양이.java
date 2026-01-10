c5eb0f426fbdc213f4080ef81627405d9f92bf83
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-01-11 00:22:44 +0900

    [Bronze V] Title: 고양이, Time: 56 ms, Memory: 11228 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) throws Exception {
+        StringBuilder sb = new StringBuilder();
+        sb.append("\\    /\\").append("\n")
+                .append(" )  ( \')").append("\n")
+                .append("(  /  )").append("\n")
+                .append(" \\(__)|");
+        System.out.println(sb);
+
+    }
+}
