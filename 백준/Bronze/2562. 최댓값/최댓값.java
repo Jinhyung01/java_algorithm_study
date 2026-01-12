84dd8f2333d47354e8438ac1f355e19c9ea6bfea
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-01-12 20:15:37 +0900

    [Bronze III] Title: 최댓값, Time: 60 ms, Memory: 11480 KB -BaekjoonHub

---
+++ 백준/Bronze/2562. 최댓값/최댓값.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int max = Integer.MIN_VALUE;
+        int index = 0;
+
+        for(int i=0; i<9;i++){
+            int num = Integer.parseInt(br.readLine());
+            if(num>max){
+                max = num;
+                index = i;
+            }
+        }
+        System.out.println(max);
+        System.out.print(index + 1);
+    }
+}
+
+
