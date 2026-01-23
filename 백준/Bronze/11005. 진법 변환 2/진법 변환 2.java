dc1654f3c03766d898ba66e3689d4a6f8aa47369
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-01-23 22:14:46 +0900

    [Bronze I] Title: 진법 변환 2, Time: 64 ms, Memory: 11544 KB -BaekjoonHub

---
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+        StringBuilder sb = new StringBuilder();
+
+        while (N>0) {
+            int remainder = N % B;
+
+            if (remainder >= 10) {
+                sb.append((char) (remainder - 10 + 'A'));
+            } else {
+                sb.append(remainder);
+            }
+            N = N / B;
+        }
+        System.out.println(sb.reverse());
+    }
+}
