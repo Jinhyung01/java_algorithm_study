c6e2098cf5d6512d537c0ff3ddbdb905258aab36
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-01-26 19:39:41 +0900

    [Bronze I] Title: 소인수분해, Time: 96 ms, Memory: 11688 KB -BaekjoonHub

---
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int i = 2;
+        while (N > 1) {
+            if (N % i == 0) {
+                sb.append(i).append("\n");
+                N /= i;
+            } else {
+                i++;
+            }
+        }
+        System.out.println(sb);
+
+    }
+}
