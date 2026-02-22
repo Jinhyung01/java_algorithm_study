21057f4646c9509f1c12585cd96247d43f613dd7
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-02-23 02:33:03 +0900

    [Silver II] Title: 스택 수열, Time: 192 ms, Memory: 26768 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -7,29 +7,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         int n = Integer.parseInt(br.readLine());
-        int cnt = 0;
+        int cnt = 1;
 
+        boolean possible = true;
         Deque<Integer> stack = new ArrayDeque<>();
 
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(br.readLine());
-            if (num - cnt > 0) {
-                for (int k = cnt + 1; k <= num; k++) {
-                    stack.offerLast(k);
-                    cnt++;
-                    sb.append("+\n");
-                }
+
+            while (cnt <= num) {
+                stack.offerLast(cnt);
+                sb.append("+\n");
+                cnt++;
+            }
+            if (!stack.isEmpty() && stack.peekLast() == num) {
                 stack.pollLast();
                 sb.append("-\n");
             } else {
-                if (stack.peekLast() != num) {
-                    System.out.println("NO");
-                    return;
-                }
-                stack.pollLast();
-                sb.append("-\n");
+                possible = false;
+                break;
             }
         }
-        System.out.println(sb);
+
+        System.out.println(possible ? sb : "NO");
     }
 }
