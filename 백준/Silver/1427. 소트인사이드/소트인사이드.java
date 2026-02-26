aeefcc121525546084fbf3c6ba1bb84bad60d95f
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-02-27 04:50:48 +0900

    [Silver V] Title: 소트인사이드, Time: 60 ms, Memory: 11524 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -1,20 +1,35 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
-        Integer[] arr = new Integer[str.length()];
+        int[] arr = new int[str.length()];
 
         for (int i = 0; i < arr.length; i++) {
             arr[i] = str.charAt(i) - '0';
         }
-        Arrays.sort(arr, Collections.reverseOrder());
 
-        for(int i=0;i<arr.length;i++){
+        for (int i = 0; i < arr.length - 1; i++) {
+            int maxIndex = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[maxIndex]) {
+                    maxIndex = j;
+                }
+            }
+            if (maxIndex != i) {
+                swap(arr, i, maxIndex);
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i]);
         }
+    }
+
+    static void swap(int[] arr, int i, int j) {
+        int tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
     }
 }
