c53d58e1707cbe0adfe8de739d75c37c7e03f5b1
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-02-27 04:42:11 +0900

    [Silver V] Title: 소트인사이드, Time: 64 ms, Memory: 11540 KB -BaekjoonHub

---
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Collections;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        Integer[] arr = new Integer[str.length()];
+
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = str.charAt(i) - '0';
+        }
+        Arrays.sort(arr, Collections.reverseOrder());
+
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]);
+        }
+    }
+}
