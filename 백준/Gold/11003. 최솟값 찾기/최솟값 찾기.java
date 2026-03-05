9cc3db35941f77c00e70252c6c4aa68996b5b499
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-03-06 02:16:16 +0900

    [Gold I] Title: 최솟값 찾기, Time: 2192 ms, Memory: 855136 KB -BaekjoonHub

--- 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.java
+++ 백준/Gold/11003. 최솟값 찾기/최솟값 찾기.java
@@ -1,47 +1,33 @@
-import java.io.*;
 import java.util.*;
-
+import java.io.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int L = Integer.parseInt(st.nextToken());
+        int[] A = new int[N];
         st = new StringTokenizer(br.readLine());
-
-        Deque<Node> deque = new ArrayDeque<>();
         for (int i = 0; i < N; i++) {
-            int now = Integer.parseInt(st.nextToken());
-            // 1. 나보다 큰 값들은 덱 뒤에서 다 빼버린다 (최솟값 유지)
-            // "어차피 내가 더 작고 더 늦게까지 살아남으니까!"
-            while (!deque.isEmpty() && deque.peekLast().value > now) {
-                deque.pollLast();
-            }
-            // 2. 현재 값을 인덱스와 함께 덱 뒤에 넣는다.
-            deque.offerLast(new Node(now,i));
-
-            // 3. 덱 맨아의 값이 윈도우 범위를 벗어났으면 뺸다.
-            // (i-L+1)이 현재 윈도우의 시작점
-            if(deque.peekFirst().index <= i-L){
-                deque.pollFirst();
-            }
-
-            // 4. 이제 덱 앞이 무조건 현재 윈도위의 최솟값
-            bw.write(deque.peekFirst().value + " ");
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        bw.flush();
-        bw.close();
-    }
 
-    static class Node {
-        public int value;
-        public int index;
+        Deque<Integer> dq = new ArrayDeque<>();
+        StringBuilder sb = new StringBuilder();
 
-        Node(int value, int index) {
-            this.value = value;
-            this.index = index;
+        for (int i = 0; i < N; i++) {
+            while (!dq.isEmpty() && A[dq.peekLast()] >=A[i]) {
+                dq.pollLast();
+            }
+            dq.offerLast(i);
+            if (dq.peekFirst() <= i - L) {
+                dq.pollFirst();
+            }
+            sb.append(A[dq.peekFirst()]).append(" ");
         }
+        System.out.println(sb);
+
     }
 }
