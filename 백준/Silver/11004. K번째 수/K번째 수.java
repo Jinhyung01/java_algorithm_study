081c84fad0086c4301480f9f1170431d074389ee
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-03-07 03:14:09 +0900

    [Silver V] Title: K번째 수, Time: 1148 ms, Memory: 501192 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -1,23 +1,68 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    static Random rand = new Random();
+
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken()) - 1;
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        int[] A = new int[N];
+
         st = new StringTokenizer(br.readLine());
-
         for (int i = 0; i < N; i++) {
-            pq.offer(Integer.parseInt(st.nextToken()));
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        for (int i = 0; i < K - 1; i++) {
-            pq.poll();
+
+        quickSelect(A, 0, N - 1, K);
+        System.out.println(A[K]);
+    }
+
+    static void quickSelect(int[] A, int S, int E, int K) {
+        if (S >= E) return;
+
+        int pivotIndex = partition(A, S, E);
+
+        if (pivotIndex == K) return;
+        else if (pivotIndex > K)
+            quickSelect(A, S, pivotIndex - 1, K);
+        else
+            quickSelect(A, pivotIndex + 1, E, K);
+    }
+
+    static int partition(int[] A, int S, int E) {
+
+        int pivotIndex = S + rand.nextInt(E - S + 1);
+        swap(A, S, pivotIndex);
+
+        int pivot = A[S];
+        int low = S + 1;
+        int high = E;
+
+        while (low <= high) {
+
+            while (low <= E && A[low] < pivot) low++;
+            while (high > S && A[high] > pivot) high--;
+
+            if (low <= high) {
+                swap(A, low, high);
+                low++;
+                high--;
+            }
         }
-        System.out.println(pq.poll());
+
+        swap(A, S, high);
+        return high;
+    }
+
+    static void swap(int[] A, int i, int j) {
+        int temp = A[i];
+        A[i] = A[j];
+        A[j] = temp;
     }
 }
