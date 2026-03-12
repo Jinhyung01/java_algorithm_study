e84397e8cf7c8d944dc84fc2da98fdeef57c8d05
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-03-13 03:59:28 +0900

    [Silver V] Title: K번째 수, Time: 1224 ms, Memory: 547064 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -1,53 +1,52 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
     static Random rand = new Random();
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         int N = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken()) - 1;
-
-        int[] A = new int[N];
-
+        int K = Integer.parseInt(st.nextToken());
         st = new StringTokenizer(br.readLine());
+        int[] A = new int[N];
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
+        quickSort(A, 0, N - 1, K - 1);
+        System.out.println(A[K - 1]);
 
-        quickSelect(A, 0, N - 1, K);
-        System.out.println(A[K]);
     }
 
-    static void quickSelect(int[] A, int S, int E, int K) {
-        if (S >= E) return;
-
-        int pivotIndex = partition(A, S, E);
-
-        if (pivotIndex == K) return;
-        else if (pivotIndex > K)
-            quickSelect(A, S, pivotIndex - 1, K);
-        else
-            quickSelect(A, pivotIndex + 1, E, K);
+    public static void quickSort(int[] A, int S, int E, int K) {
+        if (S < E) {
+            int pivotIndex = partition(A, S, E);
+            if (pivotIndex == K)
+                return;
+            else if (K < pivotIndex) {
+                quickSort(A, S, pivotIndex - 1, K);
+            } else {
+                quickSort(A, pivotIndex + 1, E, K);
+            }
+        }
     }
 
-    static int partition(int[] A, int S, int E) {
-
-        int pivotIndex = S + rand.nextInt(E - S + 1);
+    public static int partition(int[] A, int S, int E) {
+        int pivotIndex = S+ rand.nextInt(E - S + 1);
         swap(A, S, pivotIndex);
-
         int pivot = A[S];
+
         int low = S + 1;
         int high = E;
 
         while (low <= high) {
-
-            while (low <= E && A[low] < pivot) low++;
-            while (high > S && A[high] > pivot) high--;
+            while (low <= E && A[low] < pivot) {
+                low++;
+            }
+            while (high > S && A[high] > pivot) {
+                high--;
+            }
 
             if (low <= high) {
                 swap(A, low, high);
@@ -55,12 +54,11 @@ public class Main {
                 high--;
             }
         }
-
         swap(A, S, high);
         return high;
     }
 
-    static void swap(int[] A, int i, int j) {
+    public static void swap(int[] A, int i, int j) {
         int temp = A[i];
         A[i] = A[j];
         A[j] = temp;
