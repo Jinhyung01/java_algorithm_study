4a0f0a8659b025efc0875af3b7efd84904bd6957
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-03-13 04:32:31 +0900

    [Silver V] Title: K번째 수, Time: 3408 ms, Memory: 556232 KB -BaekjoonHub

--- 백준/Silver/11004. K번째 수/K번째 수.java
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -2,65 +2,22 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static Random rand = new Random();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
+        
+        ArrayList<Integer> list = new ArrayList<>(N);
+        
         st = new StringTokenizer(br.readLine());
-        int[] A = new int[N];
         for (int i = 0; i < N; i++) {
-            A[i] = Integer.parseInt(st.nextToken());
+            list.add(Integer.parseInt(st.nextToken()));
         }
-        quickSort(A, 0, N - 1, K - 1);
-        System.out.println(A[K - 1]);
-
-    }
-
-    public static void quickSort(int[] A, int S, int E, int K) {
-        if (S < E) {
-            int pivotIndex = partition(A, S, E);
-            if (pivotIndex == K)
-                return;
-            else if (K < pivotIndex) {
-                quickSort(A, S, pivotIndex - 1, K);
-            } else {
-                quickSort(A, pivotIndex + 1, E, K);
-            }
-        }
-    }
-
-    public static int partition(int[] A, int S, int E) {
-        int pivotIndex = S+ rand.nextInt(E - S + 1);
-        swap(A, S, pivotIndex);
-        int pivot = A[S];
-
-        int low = S + 1;
-        int high = E;
-
-        while (low <= high) {
-            while (low <= E && A[low] < pivot) {
-                low++;
-            }
-            while (high > S && A[high] > pivot) {
-                high--;
-            }
-
-            if (low <= high) {
-                swap(A, low, high);
-                low++;
-                high--;
-            }
-        }
-        swap(A, S, high);
-        return high;
-    }
-
-    public static void swap(int[] A, int i, int j) {
-        int temp = A[i];
-        A[i] = A[j];
-        A[j] = temp;
+        
+        Collections.sort(list);
+        
+        System.out.println(list.get(K - 1));
     }
 }
