65f40f04640fd7e8bd2c3cee6aef31fdbf89dd9b
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-03-13 09:07:30 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 660 ms, Memory: 138680 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.java
@@ -2,21 +2,35 @@ import java.io.*;
 
 public class Main {
     static int K = 2000001;
+    static int OFFSET = 1000000;
 
     public static void main(String[] args) throws Exception {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        int[] A = new int[K];
+        int[] A = new int[N];         
+        int[] count = new int[K];    
+        int[] result = new int[N];   
+        
         for (int i = 0; i < N; i++) {
-            A[Integer.parseInt(br.readLine()) + 1000000]++;
+            A[i] = Integer.parseInt(br.readLine());
+            count[A[i] + OFFSET]++;
         }
+        for (int i = 1; i < K; i++) {
+            count[i] += count[i - 1];
+        }
+
+        for (int i = N - 1; i >= 0; i--) {
+            int value = A[i];
+            int index = value + OFFSET;
+            
+            count[index]--;
+            result[count[index]] = value;
+        }
+
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < K; i++) {
-            if (A[i]-- > 0) {
-                sb.append(i - 1000000).append('\n');
-            }
+        for (int i = 0; i < N; i++) {
+            sb.append(result[i]).append('\n');
         }
         System.out.print(sb);
     }
