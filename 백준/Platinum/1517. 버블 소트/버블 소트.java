ed5e2c39caa67e9c61e6f08e19ad01571a5ea48c
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-03-13 13:00:23 +0900

    [Platinum V] Title: 버블 소트, Time: 412 ms, Memory: 95108 KB -BaekjoonHub

--- 백준/Platinum/1517. 버블 소트/버블 소트.java
+++ 백준/Platinum/1517. 버블 소트/버블 소트.java
@@ -3,51 +3,58 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static long result = 0;
-    public static int[] A;
-    public static int[] tmp;
+    public static int[] L;
+    public static int[] R;
 
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        A = new int[N];
-        tmp = new int[N];
+        int[] A = new int[N];
+
+        L = new int[N];
+        R = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
+
         mergeSort(A, 0, N - 1);
         System.out.println(result);
-
     }
 
     public static void mergeSort(int[] arr, int left, int right) {
         if (left < right) {
-            int mid = (left + right) / 2;
+            int mid = left + (right - left) / 2;
             mergeSort(arr, left, mid);
             mergeSort(arr, mid + 1, right);
-
-            merge(left, mid, right);
+            merge(arr, left, mid, right);
         }
     }
 
-    private static void merge(int left, int mid, int right) {
-        int i = left;      
-        int j = mid + 1; 
-        int k = left;      
+    private static void merge(int[] arr, int left, int mid, int right) {
+        int n1 = mid - left + 1;
+        int n2 = right - mid;
 
-        while (i <= mid && j <= right) {
-            if (A[i] <= A[j]) {
-                tmp[k++] = A[i++];
+        for (int i = 0; i < n1; i++) {
+            L[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            R[j] = arr[mid + 1 + j];
+        }
+
+        int i = 0, j = 0, k = left;
+
+        while (i < n1 && j < n2) {
+            if (L[i] <= R[j]) {
+                arr[k++] = L[i++];
             } else {
-                tmp[k++] = A[j++];
-                result += (mid - i + 1);
+                arr[k++] = R[j++];
+                result += (n1 - i);
             }
         }
-        while (i <= mid) tmp[k++] = A[i++];
-        while (j <= right) tmp[k++] = A[j++];
 
-        for (int l = left; l <= right; l++) {
-            A[l] = tmp[l];
-        }
+        while (i < n1) arr[k++] = L[i++];
+        while (j < n2) arr[k++] = R[j++];
     }
 }
