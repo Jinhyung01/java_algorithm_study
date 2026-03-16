31702b25a44a626e90dd5aacc17ba219ad2397f8
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-03-16 15:06:52 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 1608 ms, Memory: 481936 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.java
@@ -4,53 +4,18 @@ public class Main {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
+        int[] count = new int[10001];
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
+            int num = Integer.parseInt(br.readLine());
+            count[num]++;
         }
-        radixSort(arr);
-        
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(arr[i]).append('\n');
+        for (int i = 0; i < 10001; i++) {
+            while (count[i]-- > 0) {
+                sb.append(i).append('\n');
+            }
         }
         System.out.println(sb);
 
-    }
-
-    public static void radixSort(int[] arr) {
-        int max = getMax(arr);
-        for (int exp = 1; max / exp > 0; exp *= 10) {
-            countingSortByDigit(arr, exp);
-        }
-    }
-
-    private static void countingSortByDigit(int[] arr, int exp) {
-        int n = arr.length;
-        int count[] = new int[10];
-        int output[] = new int[n];
-
-        for (int i = 0; i < n; i++) {
-            int digit = (arr[i] / exp) % 10;
-            count[digit]++;
-        }
-        for (int i = 1; i < 10; i++) {
-            count[i] += count[i - 1];
-        }
-        for (int i = n - 1; i >= 0; i--) {
-            int digit = (arr[i] / exp) % 10;
-            output[--count[digit]] = arr[i];
-        }
-        System.arraycopy(output, 0, arr, 0, n);
-    }
-
-    private static int getMax(int[] tmp) {
-        int max = tmp[0];
-        for (int j : tmp) {
-            if (j > max) {
-                max = j;
-            }
-        }
-        return max;
     }
 }
