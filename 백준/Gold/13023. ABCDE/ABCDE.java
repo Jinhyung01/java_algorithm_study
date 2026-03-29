b6ed057c6f9eae68f3bb4c6e7b2a9304d0bbec4f
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-03-29 20:12:38 +0900

    [Gold V] Title: ABCDE, Time: 180 ms, Memory: 18884 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -0,0 +1,61 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    static ArrayList<Integer>[] adj;
+    static int N;
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        adj = new ArrayList[N];
+        for (int i = 0; i < N; i++) {
+            adj[i] = new ArrayList<>();
+        }
+        visited = new boolean[N];
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            addEdge(a, b);
+        }
+
+        boolean isExist = false;
+        for (int i = 0; i < N; i++) {
+            if (dfs(1, i)) {
+                isExist = true;
+                break;
+            }
+            Arrays.fill(visited, false);
+        }
+        System.out.println(isExist ? 1 : 0);
+
+    }
+
+    private static boolean dfs(int depth, int cur) {
+        if (depth == 5) {
+            return true;
+        }
+        visited[cur] = true;
+        for (int next : adj[cur]) {
+            if (!visited[next]) {
+                if (dfs(depth + 1, next)) {
+                    return true;
+                }
+            }
+        }
+        visited[cur] = false;
+        return false;
+    }
+
+    private static void addEdge(int u, int v) {
+        adj[u].add(v);
+        adj[v].add(u);
+    }
+}
