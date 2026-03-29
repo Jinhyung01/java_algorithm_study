fc3498956565daa027c94d415c8f16ec15924f71
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-03-29 17:02:31 +0900

    [Gold V] Title: 신기한 소수, Time: 68 ms, Memory: 11560 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.java
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.java
@@ -2,36 +2,38 @@ import java.io.*;
 
 public class Main {
 
-    static int N;
     static int[] arr = {2, 3, 5, 7};
+    static int N;
     static StringBuilder sb = new StringBuilder();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < arr.length; i++) {
-            dfs(arr[i], 1);
+            amazingPrime(0, arr[i]);
         }
-        System.out.println(sb);
+
+        System.out.print(sb);
     }
 
-    private static void dfs(int cur, int depth) {
-        if (depth == N) {
-            sb.append(cur).append("\n");
+    private static void amazingPrime(int depth, int num) {
+        if (depth == N - 1) {
+            sb.append(num).append('\n');
             return;
         }
         for (int i = 1; i <= 9; i += 2) {
-            int nNum = cur * 10 + i;
-            if (isPrime(nNum)) {
-                dfs(nNum, depth + 1);
+            int tmp = num * 10 + i;
+            if (isPrime(tmp)) {
+                amazingPrime(depth + 1, tmp);
             }
         }
     }
 
-    private static boolean isPrime(int n) {
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
+    public static boolean isPrime(int n) {
+        for (int i = 2; i * i <= n; i++) {
+            if (n % i == 0)
                 return false;
-            }
         }
         return true;
     }
