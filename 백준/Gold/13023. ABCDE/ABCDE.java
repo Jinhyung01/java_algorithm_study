d9acadfe1b672c149fa9c24c5605fde736aa64a1
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-03-29 20:25:26 +0900

    [Gold V] Title: ABCDE, Time: 172 ms, Memory: 19796 KB -BaekjoonHub

--- 백준/Gold/13023. ABCDE/ABCDE.java
+++ 백준/Gold/13023. ABCDE/ABCDE.java
@@ -2,10 +2,10 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
     static ArrayList<Integer>[] adj;
     static int N;
     static boolean[] visited;
-    static boolean isExist = false;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,29 +25,32 @@ public class Main {
             int b = Integer.parseInt(st.nextToken());
             addEdge(a, b);
         }
+
+        boolean isExist = false;
         for (int i = 0; i < N; i++) {
-            dfs(1, i);
-            if (isExist)
+            if (dfs(1, i)) {
+                isExist = true;
                 break;
+            }
         }
-        Arrays.fill(visited, false);
         System.out.println(isExist ? 1 : 0);
+
     }
 
-
-    private static void dfs(int depth, int cur) {
+    private static boolean dfs(int depth, int cur) {
         if (depth == 5) {
-            isExist = true;
-            return;
+            return true;
         }
         visited[cur] = true;
         for (int next : adj[cur]) {
             if (!visited[next]) {
-                dfs(depth + 1, next);
-                if(isExist) return;
+                if (dfs(depth + 1, next)) {
+                    return true;
+                }
             }
         }
         visited[cur] = false;
+        return false;
     }
 
     private static void addEdge(int u, int v) {
