3c8f678d48462527b9f24ab1898fb2771526121b
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-03-31 21:32:48 +0900

    [Gold III] Title: 나머지 합, Time: 424 ms, Memory: 167924 KB -BaekjoonHub

--- 백준/Gold/10986. 나머지 합/나머지 합.java
+++ 백준/Gold/10986. 나머지 합/나머지 합.java
@@ -1,32 +1,33 @@
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
+        long[] C = new long[M];
+        long[] S = new long[N + 1];
         st = new StringTokenizer(br.readLine());
 
-        long[] S = new long[N + 1];
-        long[] C = new long[M];
-
-        long result = 0;
-        for (int i = 1; i < S.length; i++) {
+        for (int i = 1; i <= N; i++) {
             S[i] = S[i - 1] + Integer.parseInt(st.nextToken());
         }
-        for (int i = 1; i < S.length; i++) {
-            int remain = (int) (S[i] % M);
-            if (remain == 0) result++;
-            C[remain]++;
+        long result = 0;
+        for (int i = 1; i <= N; i++) {
+            int remainder = (int) (S[i] % M);
+            if (remainder == 0) result++;
+            C[remainder]++;
         }
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < C.length; i++) {
             if (C[i] > 1) {
-                result = result + (C[i] * (C[i] - 1) / 2);
+                result = result + (C[i] * (C[i] - 1)) / 2;
             }
         }
         System.out.println(result);
+
     }
 }
