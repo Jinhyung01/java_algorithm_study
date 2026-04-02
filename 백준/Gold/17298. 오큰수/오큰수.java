654d3d2deb712a642f848d32ae3b0b20c6f2463a
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-04-03 01:09:23 +0900

    [Gold IV] Title: 오큰수, Time: 740 ms, Memory: 200920 KB -BaekjoonHub

--- 백준/Gold/17298. 오큰수/오큰수.java
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -1,32 +1,32 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        int[] A = new int[N];
-        int[] NGE = new int[N];
-        Arrays.fill(NGE,-1);
-
         StringTokenizer st = new StringTokenizer(br.readLine());
+        Deque<Integer> stack = new ArrayDeque<>();
+        int[] A = new int[N];
+        int[] result = new int[N];
+
         for (int i = 0; i < N; i++) {
             A[i] = Integer.parseInt(st.nextToken());
         }
 
-        Deque<Integer> stack = new ArrayDeque<>();
+        Arrays.fill(result, -1);
+
         for (int i = 0; i < N; i++) {
             while (!stack.isEmpty() && A[stack.peekLast()] < A[i]) {
-                NGE[stack.pollLast()] = A[i];
+                result[stack.pollLast()] = A[i];
             }
             stack.offerLast(i);
         }
+
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-            sb.append(NGE[i]).append(" ");
+            sb.append(result[i]).append(" ");
         }
         System.out.println(sb);
     }
-
-    }
+}
