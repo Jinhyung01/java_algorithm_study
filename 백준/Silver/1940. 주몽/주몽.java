778360f77449f4d70fa69e8eb9fe8ed51e2d2585
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-04-02 21:17:14 +0900

    [Silver IV] Title: 주몽, Time: 124 ms, Memory: 15056 KB -BaekjoonHub

--- 백준/Silver/1940. 주몽/주몽.java
+++ 백준/Silver/1940. 주몽/주몽.java
@@ -1,35 +1,35 @@
 import java.io.*;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
+        int[] A = new int[N];
+
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            A[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(arr);
+        Arrays.sort(A);
 
-        int left = 0;
-        int right = N - 1;
         int cnt = 0;
-
-        while (left < right) {
-            int sum = arr[left] + arr[right];
-            if (sum == M) {
+        int i = 0, j = N - 1;
+        while (i < j) {
+            int sum = A[i] + A[j];
+            if (sum < M)
+                i++;
+            else if (sum > M)
+                j--;
+            else {
                 cnt++;
-                left++;
-                right--;
-            } else if (sum < M) {
-                left++;
-            } else {
-                right--;
+                i++;
+                j--;
             }
         }
         System.out.println(cnt);
-
     }
 }
