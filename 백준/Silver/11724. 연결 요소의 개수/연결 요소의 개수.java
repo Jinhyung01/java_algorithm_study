57c2b6ab7f947c7f658678980380d96aa5e28fad
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-04-03 21:38:58 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 548 ms, Memory: 142948 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.java
@@ -4,17 +4,14 @@ import java.util.*;
 public class Main {
     static ArrayList<Integer>[] adj;
     static boolean[] visited;
-    static int componentCount = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
-
-        visited = new boolean[N + 1];
         adj = new ArrayList[N + 1];
-
+        visited = new boolean[N + 1];
         for (int i = 1; i <= N; i++) {
             adj[i] = new ArrayList<>();
         }
@@ -24,21 +21,20 @@ public class Main {
             int v = Integer.parseInt(st.nextToken());
             addEdge(u, v);
         }
-
+        int count = 0;
         for (int i = 1; i <= N; i++) {
             if (!visited[i]) {
                 dfs(i);
-                componentCount++;
+                count++;
             }
         }
-        System.out.println(componentCount);
+        System.out.println(count);
     }
 
     private static void dfs(int cur) {
         visited[cur] = true;
-
         for (int next : adj[cur]) {
-            if (!visited[next]) {
+            if(!visited[next]){
                 dfs(next);
             }
         }
