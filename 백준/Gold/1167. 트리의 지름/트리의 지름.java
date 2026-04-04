e873a81a5bdbf9bbfd304c18d88971bac2ca409a
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-04-04 23:10:41 +0900

    [Gold II] Title: 트리의 지름, Time: 796 ms, Memory: 105940 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -1,17 +1,17 @@
 import java.io.*;
 import java.util.*;
 
+
 public class Main {
     static ArrayList<Node>[] adj;
     static int[] dis;
-    static int endNode;
-    static int maxDistance = 0;
+    static int maxLength = 0;
+    static int endNode = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int V = Integer.parseInt(br.readLine());
         adj = new ArrayList[V + 1];
-
         for (int i = 1; i <= V; i++) {
             adj[i] = new ArrayList<>();
         }
@@ -21,7 +21,6 @@ public class Main {
         for (int i = 0; i < V; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
-
             int v;
             while ((v = Integer.parseInt(st.nextToken())) != -1) {
                 int d = Integer.parseInt(st.nextToken());
@@ -31,29 +30,28 @@ public class Main {
 
         bfs(1);
         Arrays.fill(dis, -1);
-        maxDistance = 0;
+
         bfs(endNode);
-        System.out.println(maxDistance);
+        System.out.println(maxLength);
+
     }
 
-
     private static void bfs(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-
+        Queue<Node> queue = new LinkedList<>();
+        queue.offer(new Node(start, 0));
         dis[start] = 0;
-        queue.offer(start);
 
         while (!queue.isEmpty()) {
-            int now = queue.poll();
-            if (dis[now] > maxDistance) {
-                maxDistance = dis[now];
-                endNode = now;
+            Node cur = queue.poll();
+            if (maxLength < dis[cur.v]) {
+                maxLength = dis[cur.v];
+                endNode = cur.v;
             }
 
-            for (Node n : adj[now]) {
-                if (dis[n.v] == -1) {
-                    dis[n.v] = dis[now] + n.dis;
-                    queue.offer(n.v);
+            for (Node next : adj[cur.v]) {
+                if (dis[next.v] == -1) {
+                    dis[next.v] = dis[cur.v] + next.dis;
+                    queue.offer(next);
                 }
             }
         }
