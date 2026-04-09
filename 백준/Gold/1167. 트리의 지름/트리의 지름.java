4cd57032070b1380226c55af2a96e8f7a2901bc9
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-04-10 02:26:04 +0900

    [Gold II] Title: 트리의 지름, Time: 756 ms, Memory: 108004 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -3,67 +3,69 @@ import java.util.*;
 
 
 public class Main {
-    static ArrayList<Node>[] adj;
     static int[] dis;
-    static int maxLength = 0;
-    static int endNode = 0;
+    static ArrayList<Node>[] adj;
+    static int endNode;
+    static int max = 0;
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int V = Integer.parseInt(br.readLine());
         adj = new ArrayList[V + 1];
+
         for (int i = 1; i <= V; i++) {
             adj[i] = new ArrayList<>();
         }
         dis = new int[V + 1];
         Arrays.fill(dis, -1);
-
         for (int i = 0; i < V; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int u = Integer.parseInt(st.nextToken());
             int v;
             while ((v = Integer.parseInt(st.nextToken())) != -1) {
-                int d = Integer.parseInt(st.nextToken());
-                adj[u].add(new Node(v, d));
+                int dis = Integer.parseInt(st.nextToken());
+                adj[u].add(new Node(v, dis));
             }
+
         }
-
         bfs(1);
-        Arrays.fill(dis, -1);
 
+        Arrays.fill(dis, -1);
         bfs(endNode);
-        System.out.println(maxLength);
+        System.out.print(max);
 
     }
 
     private static void bfs(int start) {
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(start, 0));
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.offer(start);
         dis[start] = 0;
 
         while (!queue.isEmpty()) {
-            Node cur = queue.poll();
-            if (maxLength < dis[cur.v]) {
-                maxLength = dis[cur.v];
-                endNode = cur.v;
+            int curNode = queue.poll();
+            if (max < dis[curNode]) {
+                max = dis[curNode];
+                endNode = curNode;
             }
 
-            for (Node next : adj[cur.v]) {
+            for (Node next : adj[curNode]) {
                 if (dis[next.v] == -1) {
-                    dis[next.v] = dis[cur.v] + next.dis;
-                    queue.offer(next);
+                    dis[next.v] = dis[curNode] + next.dis;
+                    queue.offer(next.v);
                 }
             }
         }
     }
 
-    public static class Node {
+    static class Node {
         int v;
         int dis;
 
         public Node(int v, int dis) {
-            this.v = v;
             this.dis = dis;
+            this.v = v;
         }
     }
 }
