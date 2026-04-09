6e5e95592851eb89632034dd3d11bab8b97cdad2
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-04-10 05:20:46 +0900

    [Silver I] Title: 최대공약수, Time: 200 ms, Memory: 59300 KB -BaekjoonHub

---
+++ 백준/Silver/1850. 최대공약수/최대공약수.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.util.*;
+
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        long A = Long.parseLong(st.nextToken());
+        long B = Long.parseLong(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+        long N = gcd(A, B);
+        for (int i = 0; i < N; i++) {
+            sb.append(1);
+        }
+        System.out.println(sb);
+    }
+
+    private static long gcd(long a, long b) {
+        while (b != 0) {
+            long r = a % b;
+            a = b;
+            b = r;
+        }
+        return a;
+    }
+
+}
