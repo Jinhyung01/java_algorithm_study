442196d8739db1f7bed67e111e57438d0d9ca6f2
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-04-13 12:00:42 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 64 ms, Memory: 11540 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -2,28 +2,26 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static final int START = 11111111;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine(), "-");
-        int n = st.countTokens();
-        int[] nums = new int[n];
 
-        for (int i = 0; i < n; i++) {
+        int result = START;
+        while (st.hasMoreTokens()) {
+            int temp = 0;
             String str = st.nextToken();
-            if (str.contains("+")) {
-                String[] s = str.split("\\+");
-                for(int j=0;j<s.length;j++){
-                    nums[i] += Integer.parseInt(s[j]);
-                }
+            String[] s = str.split("\\+");
+            for (String n : s) {
+                temp += Integer.parseInt(n);
+            }
+            if (result == START) {
+                result = temp;
             } else {
-                nums[i] = Integer.parseInt(str);
+                result -= temp;
             }
         }
-        int result = nums[0];
-        for (int i = 1; i < n; i++) {
-            result -= nums[i];
-        }
         System.out.println(result);
-
     }
 }
