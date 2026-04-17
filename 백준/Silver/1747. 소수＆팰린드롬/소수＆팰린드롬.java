5a365a24e7368e844be1934aeb423a09dcd9401c
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-04-17 12:15:37 +0900

    [Silver I] Title: 소수&팰린드롬, Time: 152 ms, Memory: 21500 KB -BaekjoonHub

--- 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
+++ 백준/Silver/1747. 소수＆팰린드롬/소수＆팰린드롬.java
@@ -1,36 +1,39 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
-        for (int i = N; ; i++) {
-            if (isPalindrome(i) && isPrime(i)) {
+        boolean[] isPrime = getSieve(10000000);
+        for (int i = N; i < isPrime.length; i++) {
+            if (isPrime[i] && isPalindrome(i)) {
                 System.out.println(i);
                 break;
             }
         }
     }
 
-    private static boolean isPrime(int n) {
-        if (n < 2) return false;
-        
-        for (int i = 2; i * i <= n; i++) {
-            if (n % i == 0)
-                return false;
+    private static boolean isPalindrome(int n) {
+        int original = n;
+        int reversed = 0;
+        while (n > 0) {
+            reversed = reversed * 10 + n % 10;
+            n /= 10;
         }
-        return true;
+        return original == reversed;
     }
 
-    private static boolean isPalindrome(int n) {
-        String str = String.valueOf(n);
-        for (int i = 0; i < str.length() / 2; i++) {
-            if (str.charAt(i) != str.charAt(str.length() - i - 1)) {
-                return false;
+    private static boolean[] getSieve(int max) {
+        boolean[] isPrime = new boolean[max + 1];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+        for (int i = 2; i * i <= max; i++) {
+            if (!isPrime[i]) continue;
+            for (int j = i * i; j <= max; j += i) {
+                isPrime[j] = false;
             }
         }
-        return true;
+        return isPrime;
     }
 }
