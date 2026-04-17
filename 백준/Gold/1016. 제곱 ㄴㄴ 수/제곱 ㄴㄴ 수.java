81d50f303bdab4f7f68178e3185395bec4060366
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-04-17 14:00:15 +0900

    [Gold I] Title: 제곱 ㄴㄴ 수, Time: 108 ms, Memory: 12700 KB -BaekjoonHub

--- 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
+++ 백준/Gold/1016. 제곱 ㄴㄴ 수/제곱 ㄴㄴ 수.java
@@ -1,34 +1,35 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         long min = Long.parseLong(st.nextToken());
         long max = Long.parseLong(st.nextToken());
-
-        boolean[] visited = new boolean[(int) (max - min + 1)];
-        int cnt = 0;
+        int size = (int) (max - min + 1);
+        boolean[] visited = new boolean[size];
 
         for (long i = 2; i * i <= max; i++) {
-            long square = i * i;
+            long temp = i * i;
+            long start = min / temp;
 
-            long start = min / square;
-            if (min % square != 0) {
+            if (min % temp != 0) {
                 start++;
             }
 
-            for (long j = start; j * square <= max; j++) {
-                visited[(int) (j * square - min)] = true;
+            for (long j = start; j * temp <= max; j++) {
+                int idx = (int) (j * temp - min);
+                if (!visited[idx])
+                    visited[idx] = true;
             }
         }
-        for (boolean i : visited) {
-            if(!i)
+        int cnt = 0;
+        for (int i = 0; i < size; i++) {
+            if (!visited[i]) {
                 cnt++;
+            }
         }
         System.out.println(cnt);
-
     }
 }
