3f50304285eb4150240daf7971943f7c4241d60b
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-04-17 13:07:27 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 72 ms, Memory: 11532 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -4,21 +4,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         long n = Long.parseLong(br.readLine());
-        long result = n;
-        
-        for (long p = 2; p * p <= n; p++) {
-            if (n % p == 0) {
-                result -= result / p;
 
-                // n에서 소인수 p를 모두 제거
-                while (n % p == 0) {
-                    n /= p;
+        long result = n;
+        for (long i = 2; i * i <= n; i++) {
+            if (n % i == 0) {
+                result = result - result / i;
+                while (n % i == 0) {
+                    n /= i;
                 }
             }
         }
         if (n > 1) {
             result -= result / n;
         }
+
         System.out.println(result);
     }
 }
