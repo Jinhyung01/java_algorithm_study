2a715647b3973c493d470479e7339ac71a4eb19b
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-04-20 14:01:56 +0900

    [Silver I] Title: 효율적인 해킹, Time: 10756 ms, Memory: 303100 KB -BaekjoonHub

--- 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
+++ 백준/Silver/1325. 효율적인 해킹/효율적인 해킹.java
@@ -2,40 +2,42 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static boolean[] visited;
+    static int[] hackCount;
     static ArrayList<Integer>[] adj;
-    static int[] counts;
-    static int[] visited;
-    static int visitToken = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N + 1];
+        hackCount = new int[N + 1];
         adj = new ArrayList[N + 1];
+
         for (int i = 1; i <= N; i++) {
             adj[i] = new ArrayList<>();
         }
-
-
         for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            int u = Integer.parseInt(st.nextToken());
-            int v = Integer.parseInt(st.nextToken());
-            adj[v].add(u);
+            int A = Integer.parseInt(st.nextToken());
+            int B = Integer.parseInt(st.nextToken());
+            adj[B].add(A);
         }
-        visited = new int[N + 1];
-        counts = new int[N + 1];
-
-        int maxVal = 0;
+        int maxHack = 0;
         for (int i = 1; i <= N; i++) {
-            visitToken++;
-            counts[i] = bfs(i);
-            maxVal = Math.max(maxVal, counts[i]);
+            Arrays.fill(visited,false);
+            int currentHack = bfs(i);
+            hackCount[i] = currentHack;
+
+            if (currentHack > maxHack) {
+                maxHack = currentHack;
+            }
         }
         StringBuilder sb = new StringBuilder();
         for (int i = 1; i <= N; i++) {
-            if (counts[i] == maxVal) {
+            if (hackCount[i] == maxHack) {
                 sb.append(i).append(" ");
             }
         }
@@ -45,13 +47,14 @@ public class Main {
     private static int bfs(int start) {
         Queue<Integer> queue = new ArrayDeque<>();
         queue.offer(start);
-        visited[start] = visitToken;
+        visited[start] = true;
         int count = 1;
+
         while (!queue.isEmpty()) {
-            int now = queue.poll();
-            for (int next : adj[now]) {
-                if (visited[next] != visitToken) {
-                    visited[next] = visitToken;
+            int cur = queue.poll();
+            for (int next : adj[cur]) {
+                if (!visited[next]) {
+                    visited[next] = true;
                     queue.offer(next);
                     count++;
                 }
@@ -59,5 +62,4 @@ public class Main {
         }
         return count;
     }
-
 }
