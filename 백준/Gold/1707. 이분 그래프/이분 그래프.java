489afb0d97eca821e3c440443d47c9091ce5d29e
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-04-20 15:03:31 +0900

    [Gold IV] Title: 이분 그래프, Time: 832 ms, Memory: 285256 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.java
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.java
@@ -8,11 +8,12 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int K = Integer.parseInt(br.readLine());
-
+        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < K; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int V = Integer.parseInt(st.nextToken());
             int E = Integer.parseInt(st.nextToken());
+
             visited = new int[V + 1];
             adj = new ArrayList[V + 1];
             for (int k = 1; k <= V; k++) {
@@ -26,39 +27,38 @@ public class Main {
                 adj[u].add(v);
                 adj[v].add(u);
             }
-            boolean isBiGraph = true;
+            boolean isBipartite = true;
+
             for (int j = 1; j <= V; j++) {
                 if (visited[j] == 0) {
-                    if (!bfs(j)) {
-                        isBiGraph = false;
+                    isBipartite = bfs(j);
+                    if (!isBipartite)
                         break;
-                    }
                 }
             }
-            System.out.println(isBiGraph ? "YES" : "NO");
+            sb.append(isBipartite ? "YES" : "NO").append("\n");
         }
-
+        System.out.println(sb);
     }
 
     private static boolean bfs(int start) {
-        Queue<Integer> queue = new LinkedList<>();
-        visited[start] = 1;
+        Queue<Integer> queue = new ArrayDeque<>();
         queue.offer(start);
+        visited[start] = 1;
 
         while (!queue.isEmpty()) {
             int cur = queue.poll();
+
             for (int next : adj[cur]) {
+                if (visited[next] == visited[cur]) {
+                    return false;
+                }
                 if (visited[next] == 0) {
                     visited[next] = visited[cur] * -1;
                     queue.offer(next);
-                } else if (visited[next] == visited[cur]) {
-                    return false;
                 }
             }
         }
         return true;
-
     }
-
-
 }
