73b65d5b4f7cf8bd7f338755b8f805be0e95871e
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-04-22 13:08:30 +0900

    [Gold IV] Title: 물통, Time: 64 ms, Memory: 11588 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.java
+++ 백준/Gold/2251. 물통/물통.java
@@ -2,9 +2,9 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+    static int[] limit = new int[3];
     static boolean[][] visited = new boolean[201][201];
     static boolean[] result = new boolean[201];
-    static int[] limit = new int[3];
     static int[] fromIdx = {0, 0, 1, 1, 2, 2};
     static int[] toIdx = {1, 2, 0, 2, 0, 1};
 
@@ -15,10 +15,9 @@ public class Main {
         limit[1] = Integer.parseInt(st.nextToken());
         limit[2] = Integer.parseInt(st.nextToken());
 
-
-        bfs();
+        dfs(0, 0);
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < result.length; i++) {
+        for (int i = 0; i <= limit[2]; i++) {
             if (result[i]) {
                 sb.append(i).append(" ");
             }
@@ -26,38 +25,28 @@ public class Main {
         System.out.println(sb);
     }
 
-    private static void bfs() {
-        Queue<int[]> queue = new ArrayDeque<>();
+    private static void dfs(int A, int B) {
+        visited[A][B] = true;
 
-        queue.offer(new int[]{0, 0, limit[2]});
-        visited[0][0] = true;
-        while (!queue.isEmpty()) {
-            int[] cur = queue.poll();
-            int A = cur[0];
-            int B = cur[1];
-            int C = cur[2];
+        if (A == 0) {
+            result[limit[2] - B] = true;
+        }
 
-            if (A == 0) {
-                result[C] = true;
+        for (int i = 0; i < 6; i++) {
+            int[] next = {A, B, limit[2] - A - B};
+            int nFrom = fromIdx[i];
+            int nTo = toIdx[i];
+
+            next[nTo] += next[nFrom];
+            next[nFrom] = 0;
+
+            if (next[nTo] > limit[nTo]) {
+                next[nFrom] =next[nTo]- limit[nTo];
+                next[nTo] = limit[nTo];
             }
 
-            for (int i = 0; i < 6; i++) {
-                int[] next = {A, B, C};
-
-                int from = fromIdx[i];
-                int to = toIdx[i];
-
-                next[to] += next[from];
-                next[from] = 0;
-                if (next[to] > limit[to]) {
-                    next[from] = next[to] - limit[to];
-                    next[to] = limit[to];
-                }
-
-                if (!visited[next[0]][next[1]]) {
-                    visited[next[0]][next[1]] =true;
-                    queue.offer(next);
-                }
+            if (!visited[next[0]][next[1]]) {
+                dfs(next[0], next[1]);
             }
         }
     }
