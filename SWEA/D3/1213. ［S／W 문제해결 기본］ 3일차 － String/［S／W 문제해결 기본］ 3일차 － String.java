d176c42a2241e30bedd4f2c708e79e396cc758ac
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-05-04 16:36:18 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 72 ms, Memory: 25,088 KB -BaekjoonHub

---
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -0,0 +1,31 @@
+import java.io.*;
+class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringBuilder sb = new StringBuilder();
+        for (int t = 1; t <= 10; t++) {
+            int T = Integer.parseInt(br.readLine());
+            char[] searchWord = br.readLine().toCharArray();
+            char[] str = br.readLine().toCharArray();
+
+            int cnt = 0;
+            for (int i = 0; i < str.length; i++) {
+                int idx = i;
+                for (int j = 0; j < searchWord.length; j++) {
+                    if (str[idx] == searchWord[j] && i != str.length - 1) {
+                        idx++;
+                    } else {
+                        break;
+                    }
+                    if (j == searchWord.length - 1) {
+                        cnt++;
+                    }
+                }
+            }
+            sb.append("#").append(T).append(" ").append(cnt).append("\n");
+        }
+        System.out.println(sb);
+
+    }
+}
