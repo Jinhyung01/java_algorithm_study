55b0c59b07ea003efb4d46f5e681c450db92de76
Jinhyung01 <129172593+Jinhyung01@users.noreply.github.com>
2026-05-07 00:51:11 +0900

    [D3] Title: [S/W 문제해결 기본] 10일차 - 비밀번호, Time: 76 ms, Memory: 25,728 KB -BaekjoonHub

--- SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.java
+++ SWEA/D3/1234. ［S／W 문제해결 기본］ 10일차 － 비밀번호/［S／W 문제해결 기본］ 10일차 － 비밀번호.java
@@ -2,38 +2,29 @@ import java.util.*;
 import java.io.*;
 
 class Solution {
-    static List<Integer> nums;
-    static int N;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
         for (int t = 1; t <= 10; t++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-            nums = new LinkedList<>();
-            String str = st.nextToken();
-            for (int i = 0; i < str.length(); i++) {
-                nums.add(str.charAt(i) - '0');
-            }
+            Stack<Character> stack = new Stack<>();
+            int N = Integer.parseInt(st.nextToken());
+            String nums = st.nextToken();
             for (int i = 0; i < N; i++) {
-                int k = 0;
-                while (i - k >= 0 && i + 1 + k < nums.size() && nums.get(i - k) == nums.get(i + 1 + k)) {
-                    k++;
-                }
-                for (int j = 0; j < 2 * k; j++) {
-                    nums.remove(i - k + 1);
-                }
-                if(k!=0){
-                    i=0;
+                char c = nums.charAt(i);
+                if (!stack.isEmpty() && stack.peek() == c) {
+                    stack.pop();
+                } else {
+                    stack.push(c);
                 }
             }
             sb.append("#").append(t).append(" ");
-            for (int i = 0; i < nums.size(); i++) {
-                sb.append(nums.get(i));
+
+            for(Character c : stack) {
+                sb.append(c);
             }
             sb.append("\n");
         }
-        System.out.println(sb);
+        System.out.print(sb);
     }
 }
